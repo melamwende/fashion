ed366025d76806e80e514924e16a89ad20e21baa
melamwende <106087619+melamwende@users.noreply.github.com>
2022-05-23 16:40:53 +0300

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+int a, b, sum;
+float average;
+
+cout<<"enter the first number";
+cin>>a;
+
+cout<<"enter the second number";
+cin>>b;
+
+sum =a + b;
+average = sum/2;
+
+cout<<"The sum of the two numbers is:"<<sum<<endl;
+cout<<"The average of the two numbers is:"<<average<<endl;
+
+return 0;
+
+}
